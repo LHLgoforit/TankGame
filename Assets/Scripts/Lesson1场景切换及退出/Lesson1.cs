59eec5af510c935b80fbf3a2e4afac9ef3e40568
Dragon <842884360@qq.com>
2025-11-24 11:22:25 +0800

    项目推送

---
+++ Assets/Scripts/Lesson1场景切换及退出/Lesson1.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region 一、场景切换
+            
+        #endregion
+        #region 二、游戏退出
+            
+        #endregion
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
